e00bf2954257e8793235c829e329729a0fcf7983
caique <caiquebrand@hotmail.com>
2021-07-28 19:11:10 -0300

    First Commit

---
+++ src/components/Principal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home'
+
+const Principal = () =>{
+    return(
+        <main className="principal">
+            <Home />
+
+        </main>
+
+    );
+
+
+};
+
+export default Principal
---
+++ src/components/Rodape.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Rodape = () => {
+    return(
+        <footer className="rodape">
+            <p>
+                Conteúdos cedidos pela NovaTec &#8212; &copy; Copyright 2020
+
+            </p>
+
+
+
+        </footer>
+
+    );
+
+
+};
+
+export default Rodape
